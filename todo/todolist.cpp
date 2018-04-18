0affdd45596472799ad9bd5466cdcce902c69b6f
Fazley Rabbi <fazleyrabbi555@gmail.com>
2018-04-19 04:54:48 +0600

    inside of showTask[show]

--- todo/todolist.cpp
+++ todo/todolist.cpp
@@ -22,5 +22,21 @@ void addTask()
 
 void showTasks()
 {
-    
+    ifstream fileRead("tasks.txt", ios::in | ios::binary);
+	if(!fileRead)
+	{
+		cout << "Cannot open file\n";
+		exit(EXIT_FAILURE);
+	}
+	struct todo myTask;
+	int i = 1;
+	while((fileRead.read((char *) &myTask, sizeof(struct todo))) != NULL)
+	{
+		cout << i++ << ": ";
+		cout << myTask.task << endl; 
+		cout << "Time: " << myTask.time << endl;
+		cout << endl;
+	}
+	cout << endl;
+	fileRead.close();
 }
