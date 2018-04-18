653db81f596a3c10910a90b6afd34af6a6b2ea3d
Fazley Rabbi <fazleyrabbi555@gmail.com>
2018-04-19 04:51:38 +0600

    inside of addTask

--- todo/todolist.cpp
+++ todo/todolist.cpp
@@ -8,5 +8,14 @@ struct todo
 
 void addTask()
 {
-    
+    ofstream fileWrite("tasks.txt", ios::app | ios::binary);
+	struct todo myTask;
+	cout << "Insert task: ";
+	cin >> myTask.task;
+	cout << "Insert time: ";
+	cin >> myTask.time;
+
+	fileWrite.write((char *) &myTask, sizeof(myTask));
+	cout << "Added!\n";
+	fileWrite.close();
 }
